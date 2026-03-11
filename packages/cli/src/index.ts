c656887870598e53da53975ee7ff5dec26438445
Bryan Kyritz <kyritzb@gmail.com>
2026-03-11 16:44:20 -0400

    release: v0.5.9

--- packages/cli/src/index.ts
+++ packages/cli/src/index.ts
@@ -12,7 +12,7 @@ const program = new Command();
 program
   .name("open-auto-doc")
   .description("Auto-generate beautiful documentation websites from GitHub repositories using AI")
-  .version("0.5.8");
+  .version("0.5.9");
 
 program
   .command("init", { isDefault: true })
